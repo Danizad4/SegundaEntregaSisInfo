33e4ddfd91f33661d26cbb263748018c884fca49
Daniel Lima <d4nielcosta@gmail.com>
2024-11-17 19:59:39 -0300

    Implementação dos comentarios

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 from django.contrib import admin
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, add_comment
 
 app_name = 'blog'
 
@@ -13,5 +13,6 @@ urlpatterns = [
     path('post/create/', PostCreateView.as_view(), name='creatpost'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='updatepost'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='deletepost'),
+    path('post/<int:pk>/comment/', add_comment, name='addcomment'),
     path('admin/', admin.site.urls), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 class PostListView(ListView):
     model = Post
@@ -13,6 +13,12 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detailpost.html'
     context_object_name = 'post'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all()
+        context['comment_form'] = CommentForm()
+        return context
 
 
 class PostCreateView(CreateView):
@@ -38,3 +44,15 @@ class PostDeleteView(DeleteView):
     
 def index(request):
     return render(request, 'staticpages/index.html')
+
+def add_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user 
+            comment.save()
+            return redirect('blog:detailpost', pk=post.pk)
+    return redirect('blog:detailpost', pk=post.pk)
